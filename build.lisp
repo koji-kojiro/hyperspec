af183c97656cd5d7438ffef501a066b586eb94a4
TANI Kojiro <kojiro0531@gmail.com>
2019-02-04 09:03:25 +0900

    Set `: save-runtime-options` to `t` to prevent passing some options like `--help` to SBCL itself

--- build.lisp
+++ build.lisp
@@ -1,3 +1,7 @@
 (pushnew (truename ".") asdf:*central-registry*)
 (ql:quickload :hyperspec :silent t)
-(sb-ext:save-lisp-and-die #P"clhs" :toplevel #'hs::main :executable t)
+(sb-ext:save-lisp-and-die 
+  #P"clhs"
+  :toplevel #'hs::main
+  :executable t
+  :save-runtime-options t)
